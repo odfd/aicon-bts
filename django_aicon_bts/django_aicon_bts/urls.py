91ff319829d0ce0a7047bcf7fee08668837a33a5
odfd <olivierdikken@gmail.com>
2023-11-26 17:26:15 +0100

    base url is now homepage, instead of /app

--- django_aicon_bts/django_aicon_bts/urls.py
+++ django_aicon_bts/django_aicon_bts/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('bts_app.urls'), name='app'),
+    path('', include('bts_app.urls'), name='app'),
 ]
