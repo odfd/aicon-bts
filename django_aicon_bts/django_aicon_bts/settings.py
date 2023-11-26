59da6a1a272346d30b567720f9f9093127e20e9e
odfd <olivierdikken@gmail.com>
2023-11-26 13:55:47 +0100

    settings py updated and works for local dev

--- django_aicon_bts/django_aicon_bts/settings.py
+++ django_aicon_bts/django_aicon_bts/settings.py
@@ -18,6 +18,13 @@ import os
 import sys
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
+
+try:
+    from .local import *
+except ImportError:
+    pass
+
+
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 DEBUG = os.getenv("DEBUG", "False") == "True"
