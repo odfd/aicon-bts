54d9b9d4f23dc16ba7e6a5446bd240bc7b785180
Olivier <olivierdikken@gmail.com>
2023-11-28 18:21:42 +0100

    WIP show loading, split into steps, show only one at a time

--- django_aicon_bts/bts_app/urls.py
+++ django_aicon_bts/bts_app/urls.py
@@ -1,10 +1,11 @@
 # myapp/urls.py
 from django.urls import path
-from .views import main_view, get_image, generate_image, process_audio
+from .views import main_view, get_image, generate_image, process_audio, get_gpt_context_prompt
 
 urlpatterns = [
     path('', main_view, name='main_view'),
     path('get_image/', get_image, name='get_image'),
     path('generate_image/', generate_image, name='generate_image'),
     path('process_audio/', process_audio, name='process_audio'),
+    path('get_gpt_context_prompt/', get_gpt_context_prompt, name='get_gpt_context_prompt'),
 ]
--- django_aicon_bts/bts_app/views.py
+++ django_aicon_bts/bts_app/views.py
@@ -9,16 +9,58 @@ from datetime import date
 from django.conf import settings
 from .models import GeneratedImage, DailyStats, RecentImage, AudioFile
 from openai import OpenAI
+import re
 
 from .utils import update_image
 
 import os
 from django.core.files import File
 
+def get_gpt_context_prompt(request):
+    if request.method == 'POST':
+        result_speech2text = request.POST.get('result_speech2text')
+
+        if result_speech2text == "":
+            return HttpResponse("Error: result_speech2text cannot be empty")
+
+        client = OpenAI(api_key=settings.OPENAI_API_KEY)
+
+        try:
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful image description creator. You only answer with an image description string. When receiving a message, you answer with only a textual description of an image representing the message (no url in the answer please)."},
+                    {"role": "user", "content": result_speech2text}
+                ]
+            )
+            print(response.choices[0].message)
+            image_prompt = response.choices[0].message.content     
+            # If 'http' is in the image_prompt, then use result_speech2text instead
+            if re.search(r'http', image_prompt):
+                image_prompt = result_speech2text
+
+            # Remove 'Image:', 'Prompt:', 'Image Prompt:', 'ImagePrompt:', and '[ ]' characters
+            image_prompt = re.sub(r'(image:|Image:|Prompt:|Image Prompt:|ImagePrompt:|\[|\])', '', image_prompt)
+
+            # If there are ' or " characters, then take only the text between those characters
+            if "'" in image_prompt or '"' in image_prompt:
+                image_prompt = re.search(r'["\'](.*?)["\']', image_prompt).group(1)          
+        except Exception as e:
+            print(e)
+            image_prompt = result_speech2text
+
+        print(image_prompt)
+
+        return HttpResponse(image_prompt)
+
+    return HttpResponse("Error: result_speech2text variable not found")
+
 def generate_image(request):
     #if image_prompt is empty, return error
     if request.method == 'POST':
-        image_prompt = request.POST.get('image_prompt')
+
+        image_prompt = request.POST.get('image_prompt')        
+        
         if image_prompt == "":
             return HttpResponse("Error: image_prompt cannot be empty")
 
@@ -31,8 +73,7 @@ def generate_image(request):
             quality="standard",
             n=1,
             )
-
-        image_url = response.data[0].url
+        image_url = response.data[0].url        
 
         update_image(image_url)
 
