cc1cd931fd66ac7e7d8370e43f34140008e38ab3
Olivier <olivierdikken@gmail.com>
2023-11-29 19:59:28 +0100

    layout changes, audio record only when sufficient sound

--- django_aicon_bts/bts_app/views.py
+++ django_aicon_bts/bts_app/views.py
@@ -81,7 +81,7 @@ def generate_image(request):
     return HttpResponse("Error: image_prompt variable not found")
 
 def main_view(request):
-    return render(request, 'main_view.html')
+    return render(request, 'audio_analyze.html')
 
 
 #gets the image url of the most recent generated image
@@ -105,37 +105,85 @@ def get_image(request):
 @csrf_exempt
 @require_POST
 def process_audio(request):
-    form = AudioFileForm(request.POST, request.FILES)
-    if form.is_valid():    
+    try:
+        form = AudioFileForm(request.POST, request.FILES)
+        if form.is_valid():    
+            print('Form is valid')
+
+            #for each audiofile, get path and delete the file to make sure no audio is saved on the server
+            for audio_file in AudioFile.objects.all():            
+                audio_file_path = audio_file.file.path
+                print('Audio file path: ', audio_file_path)
+
+                #delete the wav file, only if it exists
+                if os.path.exists(audio_file_path):
+                    os.remove(audio_file_path)
+                if os.path.exists(audio_file_path+ ".wav"):
+                    os.remove(audio_file_path+ ".wav")
+
+                #delete the audio file object
+                audio_file.delete()
+
+            audio_file = form.save()
+            print('Audio file saved')
+
+            # Rename the file to have a .wav extension
+            old_path = audio_file.file.path
+            new_path = os.path.splitext(old_path)[0] + '.wav'
+            os.rename(old_path, new_path)
+            print('File renamed to: ', new_path)
+
+            # Make the API call using OpenAI Python client
+            client = OpenAI(api_key=settings.OPENAI_API_KEY)
+            transcript = client.audio.transcriptions.create(
+                model="whisper-1",
+                file=open(new_path, "rb"),
+                response_format="text"
+            )
+            print('Transcript: ', transcript)
+
+            # Return the transcript as JSON response
+            return JsonResponse({'transcript': transcript})
+
+        print('Form is not valid')
+        return JsonResponse({'error': 'Invalid form data', 'transcript': str(e)}, status=400)
+    except Exception as e:
+        print('Error: ', str(e))
+        return JsonResponse({'error': 'Internal server error', 'transcript': str(e)}, status=500)
+    
+    
+# def process_audio(request):
+#     form = AudioFileForm(request.POST, request.FILES)
+#     if form.is_valid():    
         
-        #for each audiofile, get path and delete the file to make sure no audio is saved on the server
-        for audio_file in AudioFile.objects.all():            
-            audio_file_path = audio_file.file.path
-            #delete the wav file, only if it exists
-            if os.path.exists(audio_file_path):
-                os.remove(audio_file_path)
-            if os.path.exists(audio_file_path+ ".wav"):
-                os.remove(audio_file_path+ ".wav")
-            #delete the audio file object
-            audio_file.delete()
+#         #for each audiofile, get path and delete the file to make sure no audio is saved on the server
+#         for audio_file in AudioFile.objects.all():            
+#             audio_file_path = audio_file.file.path
+#             #delete the wav file, only if it exists
+#             if os.path.exists(audio_file_path):
+#                 os.remove(audio_file_path)
+#             if os.path.exists(audio_file_path+ ".wav"):
+#                 os.remove(audio_file_path+ ".wav")
+#             #delete the audio file object
+#             audio_file.delete()
 
-        audio_file = form.save()
+#         audio_file = form.save()
 
-        # Rename the file to have a .wav extension
-        old_path = audio_file.file.path
-        new_path = os.path.splitext(old_path)[0] + '.wav'
-        os.rename(old_path, new_path)
+#         # Rename the file to have a .wav extension
+#         old_path = audio_file.file.path
+#         new_path = os.path.splitext(old_path)[0] + '.wav'
+#         os.rename(old_path, new_path)
 
 
-        # Make the API call using OpenAI Python client
-        client = OpenAI(api_key=settings.OPENAI_API_KEY)
-        transcript = client.audio.transcriptions.create(
-            model="whisper-1",
-            file=open(new_path, "rb"),
-            response_format="text"
-        )
+#         # Make the API call using OpenAI Python client
+#         client = OpenAI(api_key=settings.OPENAI_API_KEY)
+#         transcript = client.audio.transcriptions.create(
+#             model="whisper-1",
+#             file=open(new_path, "rb"),
+#             response_format="text"
+#         )
 
-        # Return the transcript as JSON response
-        return JsonResponse({'transcript': transcript})
+#         # Return the transcript as JSON response
+#         return JsonResponse({'transcript': transcript})
 
-    return JsonResponse({'error': 'Invalid form data'}, status=400)
+#     return JsonResponse({'error': 'Invalid form data'}, status=400)
